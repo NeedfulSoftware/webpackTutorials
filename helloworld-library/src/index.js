36046cf57ba4159f7f4e426ebef83dd12bcca8aa
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-05-28 19:35:40 +0100

    Updates

---
+++ helloworld-library/src/index.js
@@ -0,0 +1,5 @@
+'use strict'
+
+export function HelloWorld() {
+    return "Hello World!"
+}
--- helloworld-library/webpack.config.js
+++ helloworld-library/webpack.config.js
@@ -3,6 +3,8 @@ var path = require("path")
 module.exports = {
   entry: "./src/index.js",
   output: {
+    library: "NeedfulSoftwareHelloWorldLibrary",
+    libraryTarget: "umd",
     filename: "needfulsoftware-helloworld-library.js",
     path: path.resolve(__dirname, "dist")
   }
